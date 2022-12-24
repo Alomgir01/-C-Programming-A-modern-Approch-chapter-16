568fbd54d62e3cef1beea6f4b080f8268933e21a
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-24 14:28:18 +0600

    Add files via upload

---
+++ sturcture.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    struct
+    {
+        char name;
+        int age;
+        int number;
+        int ID;
+        double DOB;
+    } Alomgir;
+    printf(" Alomgir\t\t\t Alom\n",Alomgir.name, Alomgir.name );
+    printf(" 22\t\t\t\t 10\n",Alomgir.age,Alomgir.age);
+    printf(" 01780854572\t\t\t 01780854572\n", Alomgir. number,Alomgir.age);
+    printf(" 2022-1-60-125\t\t\t 2022-1-60-125\n",Alomgir.number,Alomgir.ID);
+    printf(" 3rd October 2000\t\t 3rd October 2000\n", Alomgir.DOB,Alomgir.DOB);
+
+    printf("Alomgir\n24\n0000000000\n2412578421784\n4523");
+
+}
+    /*struct
+    {
+        char name;
+        int age;
+        int number;
+        int ID;
+        double DOB
+    } Alom;
+{
+    printf(" 3rd October 2000  ", Alomgir.DOB);
+}
+
+    getch();
+}*/
